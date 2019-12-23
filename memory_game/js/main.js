d694ab27c5fb013424d88595668e164c036dd7c2
Vikram <vikram.sharma1024@gmail.com>
2019-12-23 14:29:41 -0500

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,7 +1,20 @@
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "king";
+const cards = ['queen', 'queen', 'king', 'king'];
+const cardsInPlay = [];
 
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardThree);
+cardOne = cards[0];
+cardsInPlay.push(cardOne);
+console.log("User Played " + cardOne);
+cardTwo = cards[2];
+cardsInPlay.push(cardTwo);
+console.log("User Played " + cardTwo);
+
+
+if(cardsInPlay.length === 2){
+	if(cardsInPlay[0] === cardsInPlay[1]){
+		alert("You found a match!");
+	}
+	else{
+		alert("Sorry, try again.");
+	}
+}
+
