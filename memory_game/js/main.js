edad6f14745a4e4a421f446da07b7127d54f2725
Vikram <vikram.sharma1024@gmail.com>
2019-12-29 11:32:28 -0500

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,19 +1,47 @@
-const cards = ['queen', 'queen', 'king', 'king'];
+const cards = [
+//queen of hearts
+{
+	rank: "queen",
+	suit: "hearts",
+	cardImage: "images/queen-of-hearts.png"
+},
+//queen of diamonds
+{
+	rank: "queen",
+	suit: "diamonds",
+	cardImage: "images/queen-of-diamonds.png"
+},
+//king of hearts
+{
+	rank: "king",
+	suit: "hearts",
+	cardImage: "images/king-of-hearts.png"
+},
+//king of diamonds
+{
+	rank: "king",
+	suit: "diamonds",
+	cardImage: "images/king-of-diamonds.png"
+}
+];
+
+
 const cardsInPlay = [];
 
 function checkForMatch() {
 	if(cardsInPlay [0] === cardsInPlay[1]){
-		console.log("You found a match!");
+		alert("You found a match!")
 	}
 	else{
-		console.log("Sorry, try again.")
+		alert("Sorry, try again!")
 	}
 }
 
 function flipCard(cardID){
 	let playCard = cardID;
-	cardsInPlay.push(playCard);
-	console.log("User flipped " + playCard);
+	cardsInPlay.push(playCard.rank);
+	console.log("User flipped " + playCard.rank + " of " + cardID.suit);
+	console.log(cardID.cardImage);
 }
 
 flipCard(cards[0]);
