556ac60d899af4b802bf5c096cc9e3681086b269
Vikram <vikram.sharma1024@gmail.com>
2020-01-08 14:13:53 -0500

    push latest file

--- memory_game/main.js
+++ memory_game/main.js
@@ -30,11 +30,11 @@ var cardsInPlay = [];
 
 function checkForMatch() {
 	if(cardsInPlay [0] === cardsInPlay[1]){
-		alert("You found a match!")
+		alert("You found a match!");
 	}
 	else{
-		alert("Sorry, try again!")
-	}
+		alert("Sorry, try again!");
+	};
 }
 
 function createBoard(){
@@ -42,12 +42,12 @@ function createBoard(){
 	for (let i=0; i < cards.length ; i++){
 		var cardElement = document.createElement('img');
 		cardElement.setAttribute('src', 'images/back.png');
-		cardElement.setAttribute('data-id', 'i');
+		cardElement.setAttribute('data-id', i);
 		cardElement.addEventListener("click", flipCard);
 		gameBoard.appendChild(cardElement);
-	}
-}
-
+	};
+};
+/*
 function flipCard(cardId) {
 	var cardID = this.setAttribute('data-id', 'i');
 	cardsInPlay.push(cards[cardID].rank);
@@ -57,9 +57,21 @@ function flipCard(cardId) {
     //cardsInPlay = [];
  };
 };
+*/
 
+//1. set cardID to index number of the cards array
+//2. push the card's rank into cardsInPlay array (cards[cardID].rank)
+//3. use this.setAttribute keyword to pull the card image from class
+//4. check for match method called on 2nd card play. 
 
+function flipCard(){
+	var cardID = this.getAttribute('data-id');
+	cardsInPlay.push(cards[cardID].rank);
+	this.setAttribute('src', cards[cardID].cardImage);
+	if(cardsInPlay.length === 2){
+		checkForMatch();
+	};
 
-
+};
 
 createBoard();
